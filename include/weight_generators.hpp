0715926e32f08e900a567b40e4aa0ab4bea02db6
Matthias Schimek <matthias.schimek@kit.edu>
2022-03-16 16:14:02 +0100

    updated weight generators

--- include/weight_generators.hpp
+++ include/weight_generators.hpp
@@ -1,37 +1,52 @@
 #pragma once
 
+#include <algorithm>
 #include <cstdint>
+#include <type_traits>
 
 namespace graphs {
 template <typename Vertex, typename Weight, typename Edge>
 struct WeightGenerator {
   using EdgeType = Edge;
-  Weight operator()(Vertex src, Vertex dst) {
-    return get_random_weight(src, dst);
+  Weight operator()(Vertex src, Vertex dst, Weight min_weight = 1,
+                    Weight max_weight = 100'000) {
+    return get_random_weight(src, dst, min_weight, max_weight);
   }
 
  private:
-  unsigned int hash(unsigned int x) {
+  // from https://stackoverflow.com/a/12996028
+  std::uint32_t hash32(std::uint32_t x) {
     x = ((x >> 16) ^ x) * 0x45d9f3b;
     x = ((x >> 16) ^ x) * 0x45d9f3b;
     x = (x >> 16) ^ x;
     return x;
   }
+  std::uint64_t hash64(std::uint64_t x) {
+    x = (x ^ (x >> 30)) * UINT64_C(0xbf58476d1ce4e5b9);
+    x = (x ^ (x >> 27)) * UINT64_C(0x94d049bb133111eb);
+    x = x ^ (x >> 31);
+    return x;
+  }
   std::size_t knuth(std::size_t x) { return x * 2654435761 % (1ull << 31); }
 
   Weight get_random_weight(const Vertex& src, const Vertex& dst,
+                           const Weight min_weight = 1,
                            const Weight max_weight = 100'000) {
+    static_assert(std::is_integral_v<Vertex>,
+                  "Vertex type must be an integer.");
+    static_assert(std::is_integral_v<Weight>,
+                  "Weight type must be an integer.");
     Weight res = 0;
-    unsigned int lower_part_src = static_cast<unsigned int>(src);
-    unsigned int lower_part_dst = static_cast<unsigned int>(dst);
-    res = (hash(lower_part_src) + hash(lower_part_dst));
-    if constexpr (sizeof(Vertex) > 4) {
+    if constexpr (sizeof(Vertex) <= 4) {
+      unsigned int lower_part_src = static_cast<unsigned int>(src);
+      unsigned int lower_part_dst = static_cast<unsigned int>(dst);
+      res = hash32((hash32(src + 111) + hash32(dst + 111)));
+    } else {
       unsigned int upper_part_src = static_cast<unsigned int>(src >> 32);
       unsigned int upper_part_dst = static_cast<unsigned int>(dst >> 32);
-      res += hash(upper_part_src) + hash(upper_part_dst);
+      res = hash64(hash64(src + 111) + hash64(dst + 111));
     }
-    return (res % max_weight) +
-           1u;  // weight 0 is not allowed for input graph edges
+    return std::max((res % max_weight), min_weight);
   }
 };
-}  // namespace graph
+}  // namespace graphs
